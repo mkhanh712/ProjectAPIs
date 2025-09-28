39129880faa4c291110fdebf0e40099a83a329a2
KujoIT <duongminhkhanh1207@gmail.com>
2025-09-28 17:50:04 +0700

    Initial Commit.

---
+++ ProjectAPIs/src/main/java/com/main/ProjectApIsApplication.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApIsApplication.class, args);
+	}
+
+}
---
+++ ProjectAPIs/src/main/java/com/main/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.main.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController{
+	public TestController() {
+		System.err.println("a");
+	}
+}
