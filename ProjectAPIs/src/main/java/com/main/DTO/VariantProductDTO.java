5ce13b398155a1f2a35fb12deefbac5ba30183ca
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-08 19:12:02 +0700

    Chỉnh sửa API get vproduct không trả về product với mỗi vproduct nữa

--- ProjectAPIs/src/main/java/com/main/DTO/VariantProductDTO.java
+++ ProjectAPIs/src/main/java/com/main/DTO/VariantProductDTO.java
@@ -3,13 +3,11 @@ package com.main.DTO;
 import java.util.List;
 import lombok.*;
 
-import com.main.entity.VariantProduct;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class VariantProductDTO {
 	private long productId;
 	private String productName;
-	private List<VariantProduct> variants;
+	private List<VariantItemDTO> variants;
 }
