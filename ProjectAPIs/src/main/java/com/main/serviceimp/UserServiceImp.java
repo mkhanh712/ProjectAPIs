a8e77f4b165e35fca0df53e05d8b31f85ea2c9b1
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-10 22:02:42 +0700

    UserServiceImp but not done yet

---
+++ ProjectAPIs/src/main/java/com/main/serviceimp/UserServiceImp.java
@@ -0,0 +1,24 @@
+package com.main.serviceimp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.main.DTO.RegisterRequestDTO;
+import com.main.entity.User;
+import com.main.repository.CartRepository;
+import com.main.repository.UserRepository;
+import com.main.service.UserService;
+
+@Service
+public class UserServiceImp implements UserService {
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private CartRepository cartRepository;
+	
+	@Override
+	public User registerUser(RegisterRequestDTO dto) {
+		return dto;
+	}
+	
+}
