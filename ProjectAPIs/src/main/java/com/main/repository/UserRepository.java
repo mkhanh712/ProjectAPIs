7f6e804fddac0bb6b327351277bdcc2ee34e9513
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-10 21:50:28 +0700

    Add RegisterDTO and UserRepo

---
+++ ProjectAPIs/src/main/java/com/main/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.main.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	boolean existsByUsername(String userName);
+}
