4911181117687e27e603b09f65458d3015fe79e5
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-11 23:32:25 +0700

    Edit UserRepo

--- ProjectAPIs/src/main/java/com/main/repository/UserRepository.java
+++ ProjectAPIs/src/main/java/com/main/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.main.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,7 @@ import com.main.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String userName);
 	boolean existsByUsername(String userName);
+	boolean existsByEmail(String email);
 }
