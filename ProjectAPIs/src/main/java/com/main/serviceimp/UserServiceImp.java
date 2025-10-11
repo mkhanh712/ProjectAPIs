03775b1ac8d35d6c784ce4b6969c89239e9fda93
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-12 00:21:27 +0700

    Add spring security

--- ProjectAPIs/src/main/java/com/main/serviceimp/UserServiceImp.java
+++ ProjectAPIs/src/main/java/com/main/serviceimp/UserServiceImp.java
@@ -2,10 +2,12 @@ package com.main.serviceimp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.main.DTO.RegisterRequestDTO;
 import com.main.entity.User;
 import com.main.repository.CartRepository;
+import com.main.repository.RoleRepository;
 import com.main.repository.UserRepository;
 import com.main.service.UserService;
 
@@ -15,6 +17,10 @@ public class UserServiceImp implements UserService {
 	private UserRepository userRepository;
 	@Autowired
 	private CartRepository cartRepository;
+	@Autowired
+	private RoleRepository roleRepository;
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Override
 	public User registerUser(RegisterRequestDTO dto) {
