85b81b9eb8522ca80edeb4a7efe5b4391f804f32
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-14 23:00:13 +0700

    AuthController not done yet

--- ProjectAPIs/src/main/java/com/main/controller/AuthController.java
+++ ProjectAPIs/src/main/java/com/main/controller/AuthController.java
@@ -1,5 +1,13 @@
 package com.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.main.repository.UserRepository;
+import com.main.security.JwtTokenProvider;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
