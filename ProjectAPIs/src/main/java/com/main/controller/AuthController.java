427d2f1066cd49c8512340e741d0f64a980d0930
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-15 21:26:13 +0700

    AuthController

--- ProjectAPIs/src/main/java/com/main/controller/AuthController.java
+++ ProjectAPIs/src/main/java/com/main/controller/AuthController.java
@@ -1,24 +1,41 @@
 package com.main.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.main.DTO.LoginRequestDTO;
 import com.main.repository.UserRepository;
 import com.main.security.JwtTokenProvider;
+import com.main.entity.User;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 	@Autowired
 	private UserRepository userRepository;
-	
 	@Autowired
 	private JwtTokenProvider jwtTokenProvider;
-	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody LoginRequestDTO request){
+		Optional<User> userOpt = userRepository.findByUsername(request.getUsername());
+		if(userOpt.isEmpty() ||
+			!passwordEncoder.matches(request.getPassword(), userOpt.get().getPassword())) {
+			return ResponseEntity.badRequest().body(Map.of("error", "Invalid username or password"));
+		}
+		String token = jwtTokenProvider.generateToken(userOpt.get().getUsername());
+		Map<String, Object> response = new HashMap<>();
+		response.put("token", token);
+		response.put("username", userOpt.get().getUsername());
+		return ResponseEntity.ok(response);
+		
+	}
 	
 }
