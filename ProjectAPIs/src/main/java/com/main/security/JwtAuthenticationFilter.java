3c41951b76d101da005cc35491eb1e05ab9cd7c9
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-16 23:17:52 +0700

    Changed in JwtAuFilter

--- ProjectAPIs/src/main/java/com/main/security/JwtAuthenticationFilter.java
+++ ProjectAPIs/src/main/java/com/main/security/JwtAuthenticationFilter.java
@@ -5,21 +5,29 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.main.entity.User;
+import com.main.repository.UserRepository;
+
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
-
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+    private final UserRepository userRepository;
+    
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserRepository userRepository) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -30,10 +38,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             String jwt = getJwtFromRequest(request);
             if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
                 String username = jwtTokenProvider.getUsernameFromToken(jwt);
+                
+                User user = userRepository.findByUsername(username).orElse(null);
+                if(user != null) {
+                	List<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+                		.map(role -> new SimpleGrantedAuthority(role.getName()))	
+                		.collect(Collectors.toList());
+                
                 UsernamePasswordAuthenticationToken authentication = 
-                    new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());               
+                    new UsernamePasswordAuthenticationToken(username, null, authorities);               
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
             }
         } catch (Exception ex) {
             logger.error("Could not set user authentication in security context", ex);
