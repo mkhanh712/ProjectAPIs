49db53525eab516b1721abc700de18e341c2da6a
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-19 04:14:26 +0700

    when login return list role

--- ProjectAPIs/src/main/java/com/main/controller/AuthController.java
+++ ProjectAPIs/src/main/java/com/main/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.main.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import com.main.DTO.LoginRequestDTO;
 import com.main.repository.UserRepository;
 import com.main.security.JwtTokenProvider;
+import com.main.entity.Role;
 import com.main.entity.User;
 
 @RestController
@@ -30,10 +34,16 @@ public class AuthController {
 			!passwordEncoder.matches(request.getPassword(), userOpt.get().getPassword())) {
 			return ResponseEntity.badRequest().body(Map.of("error", "Invalid username or password"));
 		}
-		String token = jwtTokenProvider.generateToken(userOpt.get().getUsername());
+		User user = userOpt.get();
+		List<String> roleNames = user.getRoles()
+				.stream()
+				.map(Role::getName)
+				.collect(Collectors.toList());
+		String token = jwtTokenProvider.generateToken(user.getUsername(), roleNames);
 		Map<String, Object> response = new HashMap<>();
 		response.put("token", token);
-		response.put("username", userOpt.get().getUsername());
+		response.put("username", user.getUsername());
+		response.put("roles", roleNames);
 		return ResponseEntity.ok(response);
 		
 	}
