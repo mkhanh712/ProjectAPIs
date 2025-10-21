f5c4b8b57f1e2d99a835a9226cc1a135eadb5290
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-21 17:44:26 +0700

    add method getuser and updateuser in service

--- ProjectAPIs/src/main/java/com/main/service/UserService.java
+++ ProjectAPIs/src/main/java/com/main/service/UserService.java
@@ -1,8 +1,12 @@
 package com.main.service;
 
 import com.main.DTO.RegisterRequestDTO;
+import com.main.DTO.UserDTO;
+import com.main.DTO.UserUpdateDTO;
 import com.main.entity.User;
 
 public interface UserService {
 	User registerUser(RegisterRequestDTO dto);
+	UserDTO getUserByUsername(String username);
+	UserDTO updateUserByUsername(String username, UserUpdateDTO dto);
 }
