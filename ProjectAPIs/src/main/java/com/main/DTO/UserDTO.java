0721de1d779cfb4c10c579ea63f368efe746d009
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-21 17:28:32 +0700

    UserDTO to return profile of user

--- ProjectAPIs/src/main/java/com/main/DTO/UserDTO.java
+++ ProjectAPIs/src/main/java/com/main/DTO/UserDTO.java
@@ -1,10 +1,15 @@
 package com.main.DTO;
 
+import java.util.List;
+
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+	private Long id;
 	private String username;
+	private String email;
+	private List<String> addresses;
 }
