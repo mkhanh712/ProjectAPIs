1a9caf5f1a9f656807c870732f7b9d54cf49c763
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-22 11:29:24 +0700

    Add feature get and update user login

--- ProjectAPIs/src/main/java/com/main/controller/UserController.java
+++ ProjectAPIs/src/main/java/com/main/controller/UserController.java
@@ -1,21 +1,25 @@
 package com.main.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.main.DTO.RegisterRequestDTO;
+import com.main.DTO.UserDTO;
+import com.main.DTO.UserUpdateDTO;
 import com.main.entity.User;
 import com.main.service.UserService;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/public")
+@RequestMapping("/api")
 public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping("/register")
+	@PostMapping("/public/register")
 	/**
 	 {
 	 	"username": "aaa",
@@ -26,4 +30,16 @@ public class UserController {
 	public User registerUser(@Valid @RequestBody RegisterRequestDTO dto) {
 		return userService.registerUser(dto);
 	}
+	
+	@GetMapping("/user/me")
+    public ResponseEntity<UserDTO> getCurrentUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(userService.getCurrentUser(username));
+    }
+
+    @PutMapping("/user/updateme")
+    public ResponseEntity<UserDTO> updateCurrentUser(@RequestBody UserUpdateDTO dto) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(userService.updateCurrentUser(username, dto));
+    }
 }
