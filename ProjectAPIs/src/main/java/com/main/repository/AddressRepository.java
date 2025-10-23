1b3fd1c450d6736b42f88631669cb07dea1204af
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-23 21:09:52 +0700

    add method findByUserId to repo

--- ProjectAPIs/src/main/java/com/main/repository/AddressRepository.java
+++ ProjectAPIs/src/main/java/com/main/repository/AddressRepository.java
@@ -1,9 +1,11 @@
 package com.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.main.entity.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-
+	List<Address> findByUserId(Long userId);
 }
