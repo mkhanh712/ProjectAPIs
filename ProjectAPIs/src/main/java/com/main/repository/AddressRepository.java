3c949364fc618b3f7734ef38e226d446e4d34fe5
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-25 19:58:25 +0700

    AddressRepo

--- ProjectAPIs/src/main/java/com/main/repository/AddressRepository.java
+++ ProjectAPIs/src/main/java/com/main/repository/AddressRepository.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.main.entity.Address;
+import com.main.entity.User;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-	List<Address> findByUserId(Long userId);
+	List<Address> findByUser(User user);
 }
