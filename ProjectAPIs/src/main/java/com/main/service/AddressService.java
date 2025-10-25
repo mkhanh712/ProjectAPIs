673a461bc6e3d8224dfe2f09f3b723aca7537be3
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-25 19:57:39 +0700

    Add 4 method CRUD address

--- ProjectAPIs/src/main/java/com/main/service/AddressService.java
+++ ProjectAPIs/src/main/java/com/main/service/AddressService.java
@@ -1,5 +1,12 @@
 package com.main.service;
 
+import java.util.List;
+
+import com.main.DTO.AddressDTO;
+
 public interface AddressService {
-	
+	List<AddressDTO> getAddressesOfUser(String username);
+	AddressDTO addAddressUser(String username, AddressDTO dto);
+	AddressDTO updateAddressUser(String username, Long addressId, AddressDTO dto);
+	void deleteAddress(String username, Long addressId);
 }
