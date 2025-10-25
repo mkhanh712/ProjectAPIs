57b3f1cfa242e6d91a0de1f627c6e6c9e4e00fad
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-25 19:58:41 +0700

    add id to DTO

--- ProjectAPIs/src/main/java/com/main/DTO/AddressDTO.java
+++ ProjectAPIs/src/main/java/com/main/DTO/AddressDTO.java
@@ -5,11 +5,12 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AddressDTO {
+	private Long id;
 	private String street;
 	private String city;
 	private String state;
 	private String country;
 	private String zipcode;
-	
 }
