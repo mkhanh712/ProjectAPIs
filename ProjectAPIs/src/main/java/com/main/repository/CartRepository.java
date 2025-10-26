7356d75035eebbc6635d283ab4e0809375a97ac5
KujoIT <duongminhkhanh1207@gmail.com>
2025-10-26 22:06:28 +0700

    CartRepo

--- ProjectAPIs/src/main/java/com/main/repository/CartRepository.java
+++ ProjectAPIs/src/main/java/com/main/repository/CartRepository.java
@@ -1,11 +1,14 @@
 package com.main.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.main.entity.Cart;
+import com.main.entity.User;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-
+	Optional<Cart> findByUser(User user);
 }
