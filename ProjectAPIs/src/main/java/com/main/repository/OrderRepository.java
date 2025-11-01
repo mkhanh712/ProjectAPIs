c430c7c98d5093fd296b15fae760cdf976a5095c
KujoIT <duongminhkhanh1207@gmail.com>
2025-11-01 22:21:25 +0700

    OrderRepo done

--- ProjectAPIs/src/main/java/com/main/repository/OrderRepository.java
+++ ProjectAPIs/src/main/java/com/main/repository/OrderRepository.java
@@ -1,12 +1,15 @@
 package com.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 import com.main.entity.Order;
+import com.main.entity.User;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+	List<Order> findByUser(User user);
 }
